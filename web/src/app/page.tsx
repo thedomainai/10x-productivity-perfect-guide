6f17efa76a55e5af58e544b40ee9a506411e3651
yuta <nakabayashi@thedomainai.com>
2026-02-14 20:26:53 +0900

    auto: web/src/app/page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
 
       {/* README Content */}
       <section className="py-20 px-6">
-        <div className="max-w-4xl mx-auto">
+        <div className="max-w-5xl mx-auto">
           <MarkdownContent content={contentWithoutTitle} />
         </div>
       </section>
